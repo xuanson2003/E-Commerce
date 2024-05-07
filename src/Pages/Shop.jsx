b8ef1a24482968c06caa9683e93fd539c7e1fab6
Xuan Son <doxuanson12b2003@gmail.com>
2024-05-07 09:19:09 +0700

    feat: Add NewsLetter component

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '~/Components/Hero/Hero';
 import NewCollections from '~/Components/NewCollections/NewCollections';
+import NewsLetter from '~/Components/NewsLetter/NewsLetter';
 import Offers from '~/Components/Offers/Offers';
 import Popular from '~/Components/Popular/Popular';
 
@@ -11,6 +12,7 @@ function Shop() {
             <Popular />
             <Offers />
             <NewCollections />
+            <NewsLetter />
         </div>
     );
 }
