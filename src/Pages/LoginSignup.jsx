cd4ea815c2e4439b3adfdeded2eba2fd36728d52
Xuan Son <doxuanson12b2003@gmail.com>
2024-05-07 16:56:06 +0700

    feat: Complete UI for LoginSignup and ShopCategory components

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import './CSS/LoginSignup.scss';
 
 function LoginSignup() {
-    return <div></div>;
+    return (
+        <div className="login-signup">
+            <div className="login-signup-container">
+                <h1>Sign Up</h1>
+                <div className="login-signup-fields">
+                    <input type="text" placeholder="Your Name" />
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />
+                </div>
+                <button>Continue</button>
+                <p className="login-signup-login">
+                    Already have an account? <span>Login here</span>
+                </p>
+
+                <div className="login-signup-agree">
+                    <input type="checkbox" name="" id="privacy-policy" />
+                    <label htmlFor="privacy-policy">By continuing, i agree to the terms of use & privacy policy</label>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default LoginSignup;
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import './CSS/ShopCategory.scss';
+import { ShopContext } from '~/Context/ShopContext';
+import men_banner from '~/Components/Assets/banner_mens.png';
+import women_banner from '~/Components/Assets/banner_women.png';
+import kid_banner from '~/Components/Assets/banner_kids.png';
+import dropdown_icon from '~/Components/Assets/dropdown_icon.png';
+import Item from '~/Components/Item/Item';
 
-const ShopCategory = ({ category }) => {
-    return <div>Category pages {category}</div>;
+const ShopCategory = (props) => {
+    const { all_product } = useContext(ShopContext);
+    const categoryImages = {
+        men: men_banner,
+        women: women_banner,
+        kid: kid_banner,
+    };
+
+    const srcImg = categoryImages[props.category] || men_banner;
+
+    return (
+        <div className="shop-category">
+            <img className="shop-category-banner" src={srcImg} alt="" />
+            <div className="shop-category-indexSort">
+                <p>
+                    <span>Showing 1-12</span> out of 36 products
+                </p>
+                <div className="shop-category-sort">
+                    Sort by <img src={dropdown_icon} alt="" />
+                </div>
+            </div>
+            <div className="shop-category-products">
+                {all_product.map((item, index) => {
+                    if (props.category === item.category) {
+                        return <Item key={index} data={item} />;
+                    } else {
+                        return null;
+                    }
+                })}
+            </div>
+            <div className="shop-category-loadmore">Explore More</div>
+        </div>
+    );
 };
 
 export default ShopCategory;
